c5035629e236e669a1918aed30f7afd3ca0374d5
Andrew Bruce <andrew@camelpunch.com>
2010-09-04 17:19:11 +0100

    return nil from Song#seven_digital_url if Song#seven_digital_id is nil

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,8 @@ class Song < OpenStruct
   end
 
   def seven_digital_url
-    url = "http://api.7digital.com/1.2/track/preview?trackid=#{self.seven_digital_id}&country=gb&oauth_consumer_key=#{SEVEN_DIGITAL_API_KEY}"
+    return nil if seven_digital_id.nil?
+    "http://api.7digital.com/1.2/track/preview?trackid=#{self.seven_digital_id}&country=gb&oauth_consumer_key=#{SEVEN_DIGITAL_API_KEY}"
   end
 
   def seven_digital_id
--- spec/lib/song_spec.rb
+++ spec/lib/song_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe Song do
+  describe "seven_digital_url" do
+    context "seven_digital_id is nil" do
+      subject do
+        song = Song.new({})
+        song.stub(:seven_digital_id).and_return(nil)
+        song.seven_digital_url
+      end
+
+      it { should be_nil }
+    end
+  end
+
   describe "seven_digital_id" do
     context "tracks are nil" do
       subject { Song.new({}).seven_digital_id }
