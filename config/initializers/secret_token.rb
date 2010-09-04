1711770e9fcb02403d901a29d11abb92469177c5
Andrew Bruce <andrew@camelpunch.com>
2010-09-04 12:19:47 +0100

    removing rails 3 installation

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Piracy::Application.config.secret_token = '5c54ce7facfe59af22cda75f37b225fc24b8144577fa14593c2c3215f4634df597b25fafbc1cff68e9c9ad662724f62e2c4fa899a3d90b61cebe1d6a2dc5aa93'
