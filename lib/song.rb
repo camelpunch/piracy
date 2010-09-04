b5bfd6ec1d9cf0e319b88265ed930c42039eee11
Chris Lowis <chris.lowis@gmail.com>
2010-09-04 14:36:53 +0100

    Song class return by echonest lookup with seven digital url creation

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+require 'ostruct'
+
+SEVEN_DIGITAL_API_KEY = 'musichackday'
+
+class Song < OpenStruct
+  def initialize(args)
+    super(args)
+  end
+
+  def seven_digital_url
+    url = "http://api.7digital.com/1.2/track/preview?trackid=#{self.seven_digital_id}&country=gb&oauth_consumer_key=#{SEVEN_DIGITAL_API_KEY}"
+  end
+
+  def seven_digital_id
+    self.tracks.each do |t|
+      if t['catalog'] == "7digital"
+        foreign_id = t['foreign_id'] 
+        return foreign_id.gsub('7digital:track:','')
+      end
+    end
+  end
+end
